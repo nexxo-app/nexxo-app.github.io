3c963a74b40ecc1540d0c4eeffdb6dede33199d2
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2025-12-16 10:31:26 -0300

    Rebuild entire web build

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+    apiKey: "AIzaSyAzk7pkAqm54MqnoKFaztzvqDv-EeipoPg",
+    authDomain: "nexxo-5705c.firebaseapp.com",
+    projectId: "nexxo-5705c",
+    storageBucket: "nexxo-5705c.firebasestorage.app",
+    messagingSenderId: "211770525942",
+    appId: "1:211770525942:web:719530c46d1af4e15404d7"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log(
+        '[firebase-messaging-sw.js] Received background message ',
+        payload
+    );
+    // Customize notification here
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: 'icons/Icon-192.png'
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
